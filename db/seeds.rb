afa25cd06ae2f9cfc225ee06e3748aaf52fc0365
romain caillard <mttdiamond@gmail.com>
2023-06-29 00:32:29 +0200

    step

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,35 @@
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+# --> Je copie colle le code de Thibault pour "appeler(call)" l'API donnée dans l'exo
+puts "Cleaning database..."
+sleep(2)
+List.destroy_all
+Movie.destroy_all
+
+require 'open-uri'
+require 'json'
+url = "https://tmdb.lewagon.com/movie/top_rated"
+serialized_file = URI.open(url).read
+file = JSON.parse(serialized_file)
+file["results"].each do |result|
+  Movie.create(
+    title: result["title"],
+    overview: result["overview"],
+    rating: result["vote_average"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{result["poster_path"]}"
+  )
+  puts "created #{result["title"]}"
+  sleep(1)
+end
+list_one = List.create(name: "The best movie")
+list_two = List.create(name: "All good movies")
+Bookmark.create(movie: Movie.all.first, list: list_one, comment: "testtest")
+Bookmark.create(movie: Movie.all.last, list: list_one, comment: "testtest")
+Bookmark.create(movie: Movie.all.last, list: list_two, comment: "testtest")
+
